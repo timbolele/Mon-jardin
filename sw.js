74433f14ce285aa1e19ea4f30fd7c053b6585271
timbolele <tim.audoire@yahoo.fr>
2026-09-23 09:32:17 +0200

    Refresh offline cache for V2 feature update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mon-jardin-v2-shell';
+const CACHE='mon-jardin-v3-shell';
 const APP_SHELL=['./','./index.html','./styles.css','./app.js','./storage.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
